621c274227323f7169fcd38fd02e87d93c5fb594
jhon <91237764+RuberDucky@users.noreply.github.com>
2023-09-07 17:19:27 +0500

    Check Responsive

--- lib/Home/stampPage.dart
+++ lib/Home/stampPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
 import 'appDrawer.dart';
@@ -24,6 +25,19 @@ class _StampPageState extends State<StampPage> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = ScreenUtil().screenWidth;
+    int crossAxisCount;
+
+    // Determine the appropriate crossAxisCount based on screen width
+    if (screenWidth <= 320) {
+      crossAxisCount = 2;
+    } else if (screenWidth <= 640) {
+      crossAxisCount = 3;
+    } else if (screenWidth <= 960) {
+      crossAxisCount = 4;
+    } else {
+      crossAxisCount = 5;
+    }
     return Scaffold(
       drawer: const AppDrawer(),
       appBar: AppBar(
@@ -67,126 +81,106 @@ class _StampPageState extends State<StampPage> {
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
           shrinkWrap: true,
-          //physics: NeverScrollableScrollPhysics(),
+          //          physics: NeverScrollableScrollPhysics(),
           itemCount: stamps.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
-            crossAxisSpacing: 6.0,
-            childAspectRatio: MediaQuery.of(context).devicePixelRatio * 0.198,
+            crossAxisSpacing: 6,
+            mainAxisSpacing: 6,
+            childAspectRatio: 1 / 1.5,
           ),
           itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-                onTap: () {
-                  // Handle stamp selection
-                },
-                child: SizedBox(
-                  width: 171,
-                  height: 267,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                          width: 171,
-                          height: 267,
-                          decoration: ShapeDecoration(
-                            color: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              side: const BorderSide(
-                                  width: 0.50, color: Color(0xFFE0E0E5)),
-                              borderRadius: BorderRadius.circular(15),
-                            ),
+            return Container(
+              width: 171,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                    child: Container(
+                        width: 171,
+                        height: 151,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
                           ),
                         ),
+                        child: Image.network(
+                          "https://images.pexels.com/photos/3839651/pexels-photo-3839651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                          fit: BoxFit.fill,
+                        )),
+                  ),
+                  const Text(
+                    '25 Stamp Pack',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Satoshi',
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const Text(
+                    '\$2.49',
+                    style: TextStyle(
+                      color: Color(0xFFF65734),
+                      fontSize: 20,
+                      fontFamily: 'Satoshi',
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  Container(
+                    width: 104,
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      gradient: const LinearGradient(
+                        begin: Alignment(0.00, -1.00),
+                        end: Alignment(0, 1),
+                        colors: [Color(0xFFFF8D74), Color(0xFFF65634)],
                       ),
-                      Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                          width: 171,
-                          height: 151,
-                          decoration: const ShapeDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://images.pexels.com/photos/3839651/pexels-photo-3839651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
-                              fit: BoxFit.fill,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(
-                                  width: 0.50, color: Color(0xFFE0E0E5)),
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(15),
-                                topRight: Radius.circular(15),
-                              ),
-                            ),
-                          ),
-                        ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      const Positioned(
-                        left: 8,
-                        top: 159,
-                        child: Text(
-                          '25 Stamp Pack',
+                    ),
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Buy',
                           style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
+                            color: Colors.white,
+                            fontSize: 12,
                             fontFamily: 'Satoshi',
                             fontWeight: FontWeight.w700,
                           ),
                         ),
-                      ),
-                      const Positioned(
-                        left: 8,
-                        top: 186,
-                        child: Text(
-                          '\$2.49',
-                          style: TextStyle(
-                            color: Color(0xFFF65734),
-                            fontSize: 20,
-                            fontFamily: 'Satoshi',
-                            fontWeight: FontWeight.w900,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: 34,
-                        top: 223,
-                        child: Container(
-                          width: 104,
-                          padding: const EdgeInsets.symmetric(vertical: 8),
-                          clipBehavior: Clip.antiAlias,
-                          decoration: ShapeDecoration(
-                            gradient: const LinearGradient(
-                              begin: Alignment(0.00, -1.00),
-                              end: Alignment(0, 1),
-                              colors: [Color(0xFFFF8D74), Color(0xFFF65634)],
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          child: const Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Buy',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12,
-                                  fontFamily: 'Satoshi',
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ));
+                ],
+              ),
+            );
           },
         ),
       ),
