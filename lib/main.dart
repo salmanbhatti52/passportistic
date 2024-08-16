b4a664782217d417fdab209f6e7a479d349441ea
jhon <91237764+RuberDucky@users.noreply.github.com>
2024-08-16 16:27:18 +0500

    chore: Update Flutter SDK path to the latest version

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -48,13 +47,13 @@ void main() async {
   bool shownOnboarding = _prefs?.getBool('shownOnboarding') ?? false;
   Stripe.publishableKey = stripeTestKey;
   await Stripe.instance.applySettings();
-  // runApp(MyApp(shownOnboarding: shownOnboarding));
-  runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) => MyApp(shownOnboarding: shownOnboarding),
-    ),
-  );
+  runApp(MyApp(shownOnboarding: shownOnboarding));
+  // runApp(
+  //   DevicePreview(
+  //     enabled: true,
+  //     builder: (context) => MyApp(shownOnboarding: shownOnboarding),
+  //   ),
+  // );
 }
 
 class MyApp extends StatefulWidget {
